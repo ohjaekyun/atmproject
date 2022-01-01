216750100df39d47407844c1589e0e8b48846813
ojkyun <jkwind9@gmail.com>
2022-01-01 11:15:23 +0900

    Add transcation history

---
+++ atm.py
@@ -0,0 +1,12 @@
+class Atm:
+    def __init__(self):
+        pass
+
+    def verify_account(self, account):
+        pass
+
+    def verify_pin(self, card):
+        pass
+
+    def verify_admin(self, password):
+        pass
---
+++ generate_account.py
@@ -0,0 +1,4 @@
+import pickle
+from .account import Account, Card
+with open('./data/data.pkl', 'wb') as file:
+    pickle.dump()
